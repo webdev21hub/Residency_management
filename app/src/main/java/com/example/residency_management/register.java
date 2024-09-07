eee80cb3bcd1e675caa4a3f4bc64c2465f4e61dd
karthi01018 <karthick.s0526@gmail.com>
2024-09-07 20:26:37 +0530

    finished upto register page

---
+++ app/src/main/java/com/example/residency_management/register.java
@@ -0,0 +1,35 @@
+package com.example.residency_management;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class register extends AppCompatActivity {
+TextView tvlogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tvlogin=findViewById(R.id.tvlogin);
+        tvlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent lintent=new Intent(register.this,MainActivity.class);
+                startActivity(lintent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/residency_management/splashscreen.java
@@ -0,0 +1,35 @@
+package com.example.residency_management;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashscreen extends AppCompatActivity {
+Handler handle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splashscreen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+     new Handler().postDelayed(new Runnable(){
+         public void run()
+         {
+Intent intent=new Intent(splashscreen.this,MainActivity.class);
+startActivity(intent);
+finish();
+         }
+
+     },3000);
+    }
+}

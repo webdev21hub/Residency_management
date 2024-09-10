589349bb00762028a88f1fb71c48b9b96f4add01
karthi01018 <karthick.s0526@gmail.com>
2024-09-10 06:14:22 +0530

    after adding few activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+
 }
